dffbb0aede447101beae89ad77d8d4f08a76fc3c
LaurieTews <laurence.tews@gmail.com>
2017-07-26 11:12:57 +1200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+$("#header-block").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    },  1500);
+});
+
+$(".navbar-nav").find("a").click(function(e) {
+    e.preventDefault();
+    var section = $(this).attr("href");
+    $("html, body").animate({
+        scrollTop: $(section).offset().top
+    },  1500);
+});
